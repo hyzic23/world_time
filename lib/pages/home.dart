39b385861f5b1bfc139283e7a6c55543cd642019
hyzic <isaac.oghenevwapo@gmail.com>
2023-06-20 22:01:15 -0400

    Added toggle to day or night bgImage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,48 +9,71 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  //Map data = {};
+  Map data = {};
 
   @override
   Widget build(BuildContext context) {
 
-    Map data = ModalRoute.of(context)!.settings.arguments as Map;
+    data = ModalRoute.of(context)!.settings.arguments as Map;
     print(data);
 
+    //Set background
+    String bgImage = data['isDaytime'] ? 'day.png' : 'night.png';
+    Color bgColor = data['isDaytime'] ? Colors.blue : Colors.indigo;
+
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(0,120.0,0,0),
-            child: Column(
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/$bgImage'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(0,120.0,0,0),
+              child: Column(
         children: [
-            TextButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text('Edit Location')),
-            SizedBox(height: 20.0,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  data['location'],
-                  style: TextStyle(
-                    fontSize: 28.0,
-                    letterSpacing: 2.0
+              TextButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: Icon(
+                      Icons.edit_location,
+                      color: Colors.grey[300],
                   ),
-                )
-              ],
-            ),
-          SizedBox(height: 20.0,),
-          Text(
-              data['time'],
-            style: TextStyle(
-              fontSize: 66.0,
-            ),
-          )
+                  label: Text(
+                      'Edit Location',
+                      style: TextStyle(
+                        color: Colors.grey[300],
+                      ),
+                  )),
+              SizedBox(height: 20.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    data['location'],
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      letterSpacing: 2.0,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            SizedBox(height: 20.0,),
+            Text(
+                data['time'],
+              style: TextStyle(
+                fontSize: 66.0,
+                color: Colors.white,
+              ),
+            )
         ],
       ),
+            ),
           )),
     );
   }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -9,6 +9,7 @@ class WorldTime {
   String time = ''; //The time in  that location
   String  flag = ''; // url to an asset flag icon
   String url = '';  //location url for api endpoint
+  bool isDaytime = false; //true or false if daytime or not
 
   WorldTime({this.location = '', this.flag = '', this. url = ''});
 
@@ -29,6 +30,7 @@ class WorldTime {
       now = now.add(Duration(hours: int.parse(offset)));
 
       //set the time property
+      isDaytime = now.hour > 6 && now. hour < 20 ? true : false;
       time = DateFormat.jm().format(now);
     }
     catch(e){
